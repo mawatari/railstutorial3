472329bfd97b5ed7ab6940031eabffc99f4c9023
Naoto Mawatari <github.mawatari@gmail.com>
2015-03-10 18:36:50 +0900

    5.3.4 subjectブロックを使ってpageはテストの主題であることをRSpecに伝えるようにした

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,20 +4,14 @@ describe "Static pages" do
 
   let(:base_title) {'Ruby on Rails Tutorial Sample App'}
 
+  subject { page }
+
   describe "Home page" do
     before { visit root_path }
 
-    it "should have the content 'Sample App'" do
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the right title" do
-      expect(page).to have_title "#{base_title}"
-    end
-
-    it "should not have a custom page title" do
-      expect(page).not_to have_title '| Home'
-    end
+    it { should have_content 'Sample App' }
+    it { should have_title "#{base_title}" }
+    it { should_not have_title '| Home' }
   end
 
   describe "Help page" do
