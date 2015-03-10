1ea37931fe9116ffc39bc49d8b7dcefe3266cb0f
Naoto Mawatari <github.mawatari@gmail.com>
2015-03-10 18:21:34 +0900

    5.3.4 beforeブロックを使ってhomeのテストの冗長な部分を修正した

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,19 +5,17 @@ describe "Static pages" do
   let(:base_title) {'Ruby on Rails Tutorial Sample App'}
 
   describe "Home page" do
+    before { visit root_path }
 
     it "should have the content 'Sample App'" do
-      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the right title" do
-      visit root_path
       expect(page).to have_title "#{base_title}"
     end
 
     it "should not have a custom page title" do
-      visit root_path
       expect(page).not_to have_title '| Home'
     end
   end
